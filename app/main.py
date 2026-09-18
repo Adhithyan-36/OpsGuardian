0a356eb11e8c36233626e91b9217aaacab793ba0
Adhithyan-36 <adhithyanmurugan205@gmail.com>
2026-09-18 22:20:50 +0530

    main.py file was updated

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-from flask import Flask
+
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
+
+# Count the requests received by our application
+REQUEST_COUNT = Counter(
+    "opsguardian_requests_total",
+    "Total number of requests received"
+)
+
+
+@app.before_request
+def count_requests():
+    from flask import request
+
+    if request.path != "/metrics":
+        REQUEST_COUNT.inc()
 
 
 @app.route("/")
@@ -13,6 +29,14 @@ def health():
     return {
         "status": "healthy"
     }
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        mimetype=CONTENT_TYPE_LATEST
+    )
 
 
 if __name__ == "__main__":
